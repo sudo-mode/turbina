47cc5b0f6c093565282278c2b566bac8ca2914d9
Julia Doroshina <up.doroshina@yandex.ru>
2020-11-01 18:24:36 +0300

    add player controllers

--- src/components/Player/PlayerController.js
+++ src/components/Player/PlayerController.js
@@ -1,8 +1,27 @@
 import './PlayerController.css';
+import '../../vendor/fonts/fonts.css';
+import playBtn from '../../images/play-icon.svg';
+import pauseBtn from '../../images/pause-icon.svg';
 
-function PlayerController () {
+function PlayerController ({ isPlaying }) {
   return (
-    <p>Контур — Хадн Дадн feat. Варя Карпова и Федя Быстров</p>
+    <>
+      <audio>
+        <source src="https://ia800905.us.archive.org/19/items/FREE_background_music_dhalius/backsound.mp3"  type="audio/mp3" />
+      </audio>
+      <div className="player__controller">
+        <button
+          className="player__control-btn"
+          type="button"
+          style={{ backgroundImage: `url(${isPlaying ? pauseBtn : playBtn})` }}
+        />
+        <p className="player__song">Контур — Хадн Дадн feat. Варя Карпова и Федя Быстров</p>
+        <div className="player__timer">0:24</div>
+        <div className="player__timeline">
+          <div className="player__progress" />
+        </div>
+      </div>
+    </>
   )
 }
 
