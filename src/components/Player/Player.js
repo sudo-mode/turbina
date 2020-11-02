745c1309497ad2351f713d9a1cc55b7d542f9e72
Julia Doroshina <up.doroshina@yandex.ru>
2020-11-02 10:59:58 +0300

    refactor player and fix switcher styles

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,21 +1,27 @@
 import { useState } from 'react';
 import './Player.css';
 import PlayerController from './PlayerController';
+import PlayerSwitcher from './PlayerSwitcher';
 import PlayerExtendBtn from './PlayerExtendBtn';
 import PlayerInfo from './PlayerInfo';
 import tracks from '../../db/tracks';
 
 function Player () {
   const [isPlayerExtend, setPlayerState] = useState(true);
+  const handleExtendClick = () => {
+    setPlayerState(!isPlayerExtend);
+  }
 
   return (
     <section className="player">
       <PlayerController
-        isPlaying={false}
-        isExtend={isPlayerExtend} 
+        link='https://ia800905.us.archive.org/19/items/FREE_background_music_dhalius/backsound.mp3'
+        isExtend={isPlayerExtend}
       />
+      <PlayerSwitcher isVisible={isPlayerExtend} />
       <PlayerExtendBtn
         isOpen={isPlayerExtend}
+        onClick={handleExtendClick}
       />
       <PlayerInfo
         isOpen={isPlayerExtend}
--- src/components/Player/PlayerController.js
+++ src/components/Player/PlayerController.js
@@ -1,18 +1,26 @@
+import { useState, useEffect, useRef } from 'react';
 import './PlayerController.css';
-import PlayerSwitcher from './PlayerSwitcher';
 import playBtn from '../../images/play-icon.svg';
 import pauseBtn from '../../images/pause-icon.svg';
 
-function PlayerController ({ isPlaying, isExtend }) {
+function PlayerController ({ link }) {
+  const [isPlaying, setPlayingState] = useState(false);
+
+  const handlePlayClick = () => {
+    setPlayingState(!isPlaying);
+  }
+
+
   return (
     <>
-      <audio>
+      {/* <audio>
         <source src="https://ia800905.us.archive.org/19/items/FREE_background_music_dhalius/backsound.mp3"  type="audio/mp3" />
-      </audio>
+      </audio> */}
       <div className="player__controller">
         <button
           className="player__control-btn"
           type="button"
+          onClick={handlePlayClick}
           style={{ backgroundImage: `url(${isPlaying ? pauseBtn : playBtn})` }}
         />
         <p className="player__song">
@@ -21,8 +29,7 @@ function PlayerController ({ isPlaying, isExtend }) {
         <span className="player__timer">
           0:24
         </span>
-        <PlayerSwitcher isVisible={isExtend} />
-        <div className="player__timeline">
+        <div className="player__timeline" >
           <div className="player__progress" />
         </div>
       </div>
