588a2b21bde1dbeaf9eeeaab4e1d50d3485cc931
Oly Erofeeva <o.leland@yandex.ru>
2020-11-02 21:46:55 +0300

    added mobile & other versions of header

---
+++ src/components/ServiceLinks.js
@@ -0,0 +1,49 @@
+function ServiceLinks() {
+  return (
+    <ul className="stream-services__links">
+      
+      <li className="stream-services__link-container">
+        <a
+          className="stream-services__link"
+          href="https://marshakbooks.ru/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Яндекс.Музыка ↗
+        </a>
+      </li>
+      <li className="stream-services__link-container">
+        <a
+          className="stream-services__link"
+          href="https://marshakbooks.ru/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Spotify ↗
+        </a>
+      </li>
+      <li className="stream-services__link-container">
+        <a
+          className="stream-services__link"
+          href="https://marshakbooks.ru/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Apple Music ↗
+        </a>
+      </li>
+      <li className="stream-services__link-container">
+        <a
+          className="stream-services__link"
+          href="https://marshakbooks.ru/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          VK Music ↗
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default ServiceLinks;
