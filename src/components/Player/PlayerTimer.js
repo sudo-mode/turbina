f0280b12e38289f5d475a20d493c5fa422af66b5
Julia Doroshina <up.doroshina@yandex.ru>
2020-11-07 01:09:40 +0300

    feat: add audioplayer

---
+++ src/components/Player/PlayerTimer.js
@@ -0,0 +1,22 @@
+import './PlayerTimer.css';
+
+function PlayerTimer ({ curTime }) {
+  const formatDuration = (duration) => {
+    let minutes = Math.floor(duration/60);
+    let seconds = Math.floor(duration % 60);
+    // TODO -- уточнить по максимальной длительности треков и форматированию времени
+    // if (minutes < 10) {minutes = '0'+minutes;}
+    if (seconds < 10) {seconds = '0'+seconds;}
+    return `${minutes}:${seconds}`;
+  }
+
+
+  
+  return (
+    <span className="player__timer">
+      {formatDuration(curTime)}
+    </span>
+  )
+}
+
+export default PlayerTimer;
