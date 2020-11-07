adf0012c46d3fe6f965d4c0df2494edc5ddaabc5
Julia Doroshina <up.doroshina@yandex.ru>
2020-11-07 21:00:51 +0300

    feat: add timeline click and mouse-move handlers

--- src/components/Player/PlayerTimeline.js
+++ src/components/Player/PlayerTimeline.js
@@ -3,11 +3,37 @@ import './PlayerTimeline.css';
 function PlayerTimeline ({ duration, curTime, onTimeUpdate }) {
   const curPercentage = (curTime / duration) * 100;
 
-  // TODO -- дописать логику установки текущего времени 
-  // в соответствии с положением бегунка
+  const calculateClickedTime = (e) => {
+    const timeline = document.querySelector('.player__timeline');
+    const timelineWidth = timeline.clientWidth;
+    const timelineStart = timeline.getBoundingClientRect().left
+    const clickedXposition = e.pageX;
+    const clickedPositionOnTimeline = clickedXposition - timelineStart;
+    const timePerPixel = duration / timelineWidth;
+    
+    return clickedPositionOnTimeline * timePerPixel;
+  }
+
+  const handleTimeDrag = (e) => {
+    onTimeUpdate(calculateClickedTime(e));
+
+    const updateTimeOnMove = eMove => {
+      onTimeUpdate(calculateClickedTime(eMove));
+    };
+
+    document.addEventListener("mousemove", updateTimeOnMove);
+
+    document.addEventListener("mouseup", function handleMouseUp () {
+      document.removeEventListener("mousemove", updateTimeOnMove);
+      document.removeEventListener('mouseup', handleMouseUp);
+    });
+  }
 
   return (
-    <div className="player__timeline">
+    <div
+    className="player__timeline"
+      onMouseDown={e => handleTimeDrag(e)}
+    >
       <div 
         className="player__progress"
         style={{ width: `${curPercentage}%` }}
