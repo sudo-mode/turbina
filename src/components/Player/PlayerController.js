a32056a7975bfac26642eab1126c39f604f5abb7
Julia Doroshina <up.doroshina@yandex.ru>
2020-11-07 18:24:06 +0300

    refactor: extract hook useTicker

--- src/components/Player/PlayerController.js
+++ src/components/Player/PlayerController.js
@@ -5,86 +5,17 @@ import PlayerTimeline from './PlayerTimeline';
 import playBtn from '../../images/play-icon.svg';
 import pauseBtn from '../../images/pause-icon.svg';
 import PlayerTimer from './PlayerTimer';
+import useTicker from '../../hooks/useTicker';
 // import PlayerTrack from './PlayerTrack';
 
 function PlayerController ({ track }) {
-  // const [isPlaying, setPlaying] = useState(false);
-
   const handlePlayClick = () => {
     setPlaying(!isPlaying);
   }
 
-  // TODO -- можно КАК-ТО использовать для паузы в бегущей строке
-  // // Функция задержки
-  // const delay = async (ms) => await new Promise(resolve => setTimeout(resolve, ms));
-
   const trackRef = useRef();
-  // Добавляем стейт переменную, определяющую необходимость запуска бегущей строки
-  const [isMarqueeNeeded, setMarqueeState] = useState(false);
-  
-  // Обработчик ресайза окна
-  const handleResize = () => {
-    const trackElement = trackRef.current;
-    const trackContainer = trackElement.parentElement;
-    const trackElementWidth = trackElement.scrollWidth;
-    const trackContainerWidth = trackContainer.clientWidth;
-    if (trackElementWidth > trackContainerWidth) {
-      setMarqueeState(true);
-    } else {
-      setMarqueeState(false);
-    }
-  }
 
-  // Проверяем необходимость запуска бегущей строки при загрузке страницы
-  useEffect(handleResize, []);
-
-  // Добавляем слышатели на ресайз окна
-  useEffect(() => {
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    }
-  }, []);
-
-  // Добавляем стейт переменную для хранения текущего номера таймера
-  const [intervalId, setIntervalId] = useState(null);
-
-  // При изменении isMarqueeNeeded запускаем или останавливаем бегущую строку
-  useEffect(() => {
-    const trackElement = trackRef.current;
-    const trackContainer = trackElement.parentElement;
-    const trackElementWidth = trackElement.scrollWidth;
-    const trackContainerWidth = trackContainer.clientWidth;
-
-    const startMarquee = () => {
-      // TODO -- пауза
-      currentX--;
-      trackElement.style.left = `${currentX}px`;
-      if (currentX === -trackElementWidth) {
-        // 10 здесь устанавливает "задержку" в пикселях переде появлением строки справа
-        currentX = trackContainerWidth + 10;
-      }
-    }
-
-    let currentX = 0;
-   
-    if (isMarqueeNeeded) {
-      trackContainer.classList.add('player__song-container_masked');
-      const currentIntervalId = setInterval(startMarquee, 10);
-      setIntervalId(currentIntervalId)
-    } else {
-      trackContainer.classList.remove('player__song-container_masked');
-      clearInterval(intervalId);
-      trackElement.style.left = '0px';
-    }
-  }, [isMarqueeNeeded])
-
-
-  // const audio = new Audio(track.link);
-
-  // console.dir(audio);
-  // const playerRef = useRef();
-  // // console.log(timeline.current);
+  useTicker(trackRef);
 
   const { curTime, duration, isPlaying, setPlaying, setClickedTime } = useAudioPlayer();
 
---
+++ src/hooks/useTicker.js
@@ -0,0 +1,78 @@
+import { useState, useEffect } from 'react';
+
+/*
+Хук useTicker реализует бегущую строку.
+Измеряет текущую ширину элемента и контейнера, определяет необходимость
+появления бегущей строки и запускает её.
+Реагирует на 'resize' окна браузера.
+elementRef -- ссылка на элемент, который становится бегущей строкой.
+containerTickerAddClass -- класс, добавляемый в список классов контейнера
+      при появлении в нём бегущей строки.
+*/
+function useTicker(elementRef, containerTickerAddClass) {
+  // стейт-переменнуя, определяющая необходимость бегущей строки
+  const [isTickerNeeded, setTickerState] = useState(false);
+  
+  // Обработчик ресайза окна
+  const handleResize = () => {
+    const element = elementRef.current;
+    const elementContainer = element.parentElement;
+    const elementWidth = element.scrollWidth;
+    const elementContainerWidth = elementContainer.clientWidth;
+    if (elementWidth > elementContainerWidth) {
+      setTickerState(true);
+    } else {
+      setTickerState(false);
+    }
+  }
+  
+  // Проверяем необходимость запуска бегущей строки при загрузке страницы
+  useEffect(handleResize, []);
+  
+  // Добавляем слышатели на ресайз окна
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  }, []);
+  
+  // Добавляем стейт переменную для хранения текущего номера таймера
+  const [intervalId, setIntervalId] = useState(null);
+  
+  // TODO -- можно КАК-ТО использовать для паузы в бегущей строке
+  // // Функция задержки
+  // const delay = async (ms) => await new Promise(resolve => setTimeout(resolve, ms));
+
+  // При изменении isTickerNeeded запускаем или останавливаем бегущую строку
+  useEffect(() => {
+    const element = elementRef.current;
+    const elementContainer = element.parentElement;
+    const elementWidth = element.scrollWidth;
+    const elementContainerWidth = elementContainer.clientWidth;
+  
+    const startTicker = () => {
+      // TODO -- пауза
+      currentX--;
+      element.style.left = `${currentX}px`;
+      if (currentX === -elementWidth) {
+        // 10 здесь устанавливает "задержку" в пикселях переде появлением строки справа
+        currentX = elementContainerWidth + 10;
+      }
+    }
+  
+    let currentX = 0;
+    
+    if (isTickerNeeded) {
+      elementContainer.classList.add('player__song-container_masked');
+      const currentIntervalId = setInterval(startTicker, 10);
+      setIntervalId(currentIntervalId)
+    } else {
+      elementContainer.classList.remove('player__song-container_masked');
+      clearInterval(intervalId);
+      element.style.left = '0px';
+    }
+  }, [isTickerNeeded]);
+}
+
+export default useTicker;
