2ee03881b572e3ca899276bd503bd0921d386f51
Julia Doroshina <up.doroshina@yandex.ru>
2020-11-12 23:16:20 +0300

    refactor(player): delete extra comments

--- src/components/Player/PlayerTimer.js
+++ src/components/Player/PlayerTimer.js
@@ -4,8 +4,6 @@ function PlayerTimer ({ duration, curTime }) {
   const formatDuration = (duration) => {
     let minutes = Math.floor(duration/60);
     let seconds = Math.floor(duration % 60);
-    // TODO -- уточнить по максимальной длительности треков и форматированию времени
-    // if (minutes < 10) {minutes = '0'+minutes;}
     if (seconds < 10) {seconds = '0'+seconds;}
     return `${minutes}:${seconds}`;
   }
