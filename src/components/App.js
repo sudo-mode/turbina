bad721669f7edba767681e3616995adb83b351d2
Oly Erofeeva <o.leland@yandex.ru>
2020-11-13 00:49:04 +0300

    Feat: support for landscape mode

--- src/components/App.js
+++ src/components/App.js
@@ -22,6 +22,7 @@ function App() {
         <Main
           onPlayerExtend={onPlayerExtend}
           isPlayerExtend={isPlayerExtend}
+          isLandscape={isLandscape}
         />
         <Info />
         <Footer
