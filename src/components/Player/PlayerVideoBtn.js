7a4d0755b6d60410ec22266fca15e07c08e8b6f2
Sergey Medvedev <medvedevsm1989@yandex.ru>
2020-11-12 16:37:36 +0300

    afasdf

--- src/components/Player/PlayerVideoBtn.js
+++ src/components/Player/PlayerVideoBtn.js
@@ -5,14 +5,17 @@ function PlayerVideo({ videoLink }) {
   const playerVideoLink = cn('player__video-link', { 'player__video-link_active': videoLink });
 
   return (
-    <a
-      className={playerVideoLink}
-      href={videoLink}
-      target="_blank"
-      rel="noreferrer"
-    >
-      <span className='player__video-icon'></span><span className='player__video-btn-text'>Клип</span>
-    </a>
+
+    <div className='player__video-link-wrapper'>
+      <a
+        className={playerVideoLink}
+        href={videoLink}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <span className='player__video-icon'></span><span className='player__video-btn-text'>Клип</span>
+      </a>
+    </div>
 
   )
 }
