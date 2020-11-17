0860101b023d4244c1a4a564d9cef8a23f2b42aa
alex-kurkov <alexkourkov@yandex.ru>
2020-11-17 15:47:52 +0500

    fix: removed logo pointer position response, swapped bg image

--- src/components/ProjectLogo.js
+++ src/components/ProjectLogo.js
@@ -1,28 +1,16 @@
-import { useRef, useEffect } from 'react';
 import './ProjectLogo.css';
 import './Blur.css';
 import logo from '../images/turbina.svg';
 import cn from 'classnames';
-import transformElement from '../utils/transformElement';
-import throttle from '../utils/throttle';
+
 
 function ProjectLogo({ isPlayerExtend, isMobile, isLandscape }) {
-  const projectLogoRef = useRef();
   const titleStyle = cn("title", { "blur": isPlayerExtend && (isMobile || isLandscape)})
-
-  // вызываем эффект смещения логотипа в сторону позиции указателя  
-  useEffect(() => {
-    window.addEventListener('mousemove',
-      (e) => throttle(transformElement(e, projectLogoRef.current, 'skew')), 50);
-    return () => window.removeEventListener('mousemove', transformElement);
-  }, []);
-
   return (
     <h1
       className={titleStyle}
     >
-      <img        
-        ref={projectLogoRef} 
+      <img
         className="title__logo"
         src={logo}
         alt="Логотип Турбина" />
--- src/utils/transformElement.js
+++ src/utils/transformElement.js
@@ -6,23 +6,10 @@ const transformElement = (evt, element, method = 'position') => {
   // вычисляем смещение относительно left/top нуля
   const x = 100 - Math.round((evt.clientX / windowWidth) * 100);
   const y = 100 - Math.round((evt.clientY / windowHeight) * 100);
-  // меняем стили элемента, в зависимости от переданного метода
-  switch (method) {
-    case 'position':
-      // ускоритель - чем меньше - тем кратно меньше смещение. НЕ УСТАНАВЛИВАТЬ
-      // БОЛЬШЕ 1! - особенности реализации фона!
-      const deflector = .6;
-      element.style.backgroundPosition = `${x * deflector}% ${y * deflector}%`;
-      break;
-    case 'skew':
-      // чем больше дефлектор сдвига - тем меньше смещение
-      const deflectorSkew = 20;
-      element.style.transform = `skew(${2.5 - (x / deflectorSkew)}deg, ${2.5 - (y / deflectorSkew)}deg)`;
-      break;
-    // здесь оставим возможность добавить еще трансформ-кейсы...
-    default:
-      return;
-  }
+  // ускоритель - чем меньше - тем кратно меньше смещение. НЕ УСТАНАВЛИВАТЬ
+  // БОЛЬШЕ 1! - особенности реализации фона!
+  const deflector = 1;
+  element.style.backgroundPosition = `${x * deflector}% ${y * deflector}%`;
 }
 
 export default transformElement
