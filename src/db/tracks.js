437b8680c09c8f77d85a5cd10447c47cf8d95ac1
Julia Doroshina <up.doroshina@yandex.ru>
2020-12-05 16:02:34 +0300

    feat: add checkout buttons

--- src/db/tracks.js
+++ src/db/tracks.js
@@ -1,4 +1,6 @@
 import loveOutline from './audio/Контур любви — Хадн дадн feat. Варя Карпова и Федя Быстров.mp3';
+import zema from './audio/Зёма-Кровосток.mp3';
+import derevnya from './audio/Деревня-Кровосток.mp3';
 import defaultBackground from '../images/background.png';
 import loveOutlineCover from '../images/love_outline_cover.jpg';
 
@@ -58,6 +60,78 @@ export const tracks = [
       Что выхожу за контур любви,
       Что выхожу.`,
     link: loveOutline,
+    videoLink: '',
+    theme: {
+      color: '#fff',
+      backgroundColor: 'white',
+      backgroundImage: `url(${defaultBackground})`,
+    },
+    albumPicture: loveOutlineCover,
+    streamings: [
+      { 
+        serviceLabel: 'Яндекс.Музыка',
+        serviceLink: 'https://music.yandex.ru/album/12769745'
+      },
+      { 
+        serviceLabel: 'Spotify',
+        serviceLink: 'https://open.spotify.com/track/0FRjmDhfKmVARMOvmUm6no?si=rwTima3JQaWdse3mrQko1g'
+      },
+      { 
+        serviceLabel: 'Apple Music',
+        serviceLink: 'https://music.apple.com/ru/album/контур-любви-feat-варя-карпова-федя-быстров-single/1539942736'
+      },
+      { 
+        serviceLabel: 'VK Music',
+        serviceLink: 'https://vk.com/music/album/-2000389059_9389059_4fb8e57c6b6b7b1795'
+      },
+    ],
+  },
+  {
+    id: '№2',
+    trackName: 'Дагестан',
+    author: 'Хадн дадн',
+    originalAuthor: 'Сабина Саидова',
+    text: `Мой милый, милый мальчик, 
+      Моя милая, милая девочка.
+      Никогда не знаешь, что будет дальше
+      В этой гадкой зиме.`,
+    link: zema,
+    videoLink: '',
+    theme: {
+      color: '#fff',
+      backgroundColor: 'white',
+      backgroundImage: `url(${defaultBackground})`,
+    },
+    albumPicture: loveOutlineCover,
+    streamings: [
+      { 
+        serviceLabel: 'Яндекс.Музыка',
+        serviceLink: 'https://music.yandex.ru/album/12769745'
+      },
+      { 
+        serviceLabel: 'Spotify',
+        serviceLink: 'https://open.spotify.com/track/0FRjmDhfKmVARMOvmUm6no?si=rwTima3JQaWdse3mrQko1g'
+      },
+      { 
+        serviceLabel: 'Apple Music',
+        serviceLink: 'https://music.apple.com/ru/album/контур-любви-feat-варя-карпова-федя-быстров-single/1539942736'
+      },
+      { 
+        serviceLabel: 'VK Music',
+        serviceLink: 'https://vk.com/music/album/-2000389059_9389059_4fb8e57c6b6b7b1795'
+      },
+    ],
+  },
+  {
+    id: '№3',
+    trackName: 'ляпапам',
+    author: 'Хадн дадн',
+    originalAuthor: 'Варя Карпова и Федя Быстров',
+    text: `Мой милый, милый мальчик, 
+      Моя милая, милая девочка.
+      Никогда не знаешь, что будет дальше
+      В этой гадкой зиме.`,
+    link: derevnya,
     videoLink: '',
     theme: {
       color: '#fff',
